54f674d49ddb7d2ac344315ef90aae909782b98a
Guy Davidson <guydav@gmail.com>
2020-04-28 17:55:36 -0400

    Fixing some bug with the color above color thing.

--- simple_relational_reasoning/datagen/object_relations.py
+++ simple_relational_reasoning/datagen/object_relations.py
@@ -152,20 +152,21 @@ class ColorAboveColorRelation(ObjectRelation):
 
         # If no objects in the above color exist, create one
         if len(above_color_y_positions) == 0:
-            idx = random.randint(0, objects.shape[0] - 1)
-            objects[idx, self.color_field_slice] = self.above_color_tensor
-            colors[idx, :] = self.above_color_tensor
-            print('Added object')
+            index_to_modify = random.randint(0, objects.shape[0] - 1)
+            objects[index_to_modify, self.color_field_slice] = self.above_color_tensor
+            colors[index_to_modify, :] = self.above_color_tensor
 
         below_color_y_positions = objects[colors.eq(self.below_color_tensor).all(dim=1), self.y_field_slice]
         max_below_color_position = int(below_color_y_positions.max())
         new_above_color_position = random.randint(max_below_color_position, self.y_field_gen.max_coord - 1)
 
         above_object_indices = torch.nonzero(colors.eq(self.above_color_tensor).all(dim=1)).squeeze()
-        print(above_object_indices)
-        index_to_modify = above_object_indices[torch.randperm(above_object_indices.shape[0])[0]]
-        objects[index_to_modify, self.y_field_slice] = new_above_color_position
+        if len(above_object_indices.shape) == 0:
+            index_to_modify = int(above_object_indices)
+        else:
+            index_to_modify = above_object_indices[torch.randperm(above_object_indices.shape[0])[0]]
 
+        objects[index_to_modify, self.y_field_slice] = new_above_color_position
         return objects
 
 
