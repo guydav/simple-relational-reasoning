b6872489b1a3f0945f7fa2ef8bbb62c62ec3c35f
Guy Davidson <guydav@gmail.com>
2020-04-30 16:33:38 -0400

    Forgot to add the new relation to __init__

--- simple_relational_reasoning/datagen/__init__.py
+++ simple_relational_reasoning/datagen/__init__.py
@@ -1,4 +1,4 @@
 from simple_relational_reasoning.datagen.object_fields import FIELD_TYPES
 from simple_relational_reasoning.datagen.object_gen import ObjectGenerator, ObjectGeneratorDataset
 from simple_relational_reasoning.datagen.object_relations import OneDAdjacentRelation, MultipleDAdjacentRelation, \
-    ColorAboveColorRelation, ObjectCountRelation
+    ColorAboveColorRelation, ObjectCountRelation, IdenticalObjectsRelation
