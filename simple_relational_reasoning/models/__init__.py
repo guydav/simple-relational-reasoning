7771449ad73b0d3b9bbe8b63988d76bee5440b43
Guy Davidson <guydav@gmail.com>
2020-05-09 12:23:04 -0400

    Some quick additions to __init__

--- simple_relational_reasoning/models/__init__.py
+++ simple_relational_reasoning/models/__init__.py
@@ -1,3 +1,5 @@
-from simple_relational_reasoning.models.base import BaseObjectModel
-from simple_relational_reasoning.models.mlp import MLPModel
+from simple_relational_reasoning.models.base import BaseObjectModel, ObjectCombinationMethod
+from simple_relational_reasoning.models.mlp import MLPModel, CombinedObjectMLPModel
 from simple_relational_reasoning.models.relation_net import RelationNetModel
+from simple_relational_reasoning.models.transformer import TransformerModel
+
