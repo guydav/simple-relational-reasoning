09dd862700ba769eca68bfa389aef468c6fa7fc4
Guy Davidson <guydav@gmail.com>
2020-11-16 12:03:24 -0500

    1. Updated some of the data analyses. 2. Implemented a simplification of the CNN model, which only passes the model channels corresponding to the one-hot, without passing it the entire object representation (x,y,width). This also only makes sense to run in cases where we're not using object sizes (as otherwise it would be impoverished).

--- run/quinn_defaults.py
+++ run/quinn_defaults.py
@@ -171,7 +171,8 @@ MODEL_CONFIGURATIONS = {
         models.CombinedObjectMLPModel: dict(embedding_size=8, prediction_sizes=[32, 32, 16]),
         models.RelationNetModel: dict(embedding_size=8, object_pair_layer_sizes=[32], combined_object_layer_sizes=[32]),
         models.TransformerModel: dict(embedding_size=8, transformer_mlp_sizes=[8, 8], mlp_sizes=[32, 32]),
-        models.CNNModel: dict(conv_sizes=[8, 16], mlp_sizes=[16, 8],)
+        # models.CNNModel: dict(conv_sizes=[8, 16], mlp_sizes=[16, 8],),
+        models.SimplifiedCNNModel: dict(conv_sizes=[8, 16], mlp_sizes=[16, 8],),
     },
     LARGER_MODELS_CONFIG_KEY: {
         models.CombinedObjectMLPModel: dict(embedding_size=16, prediction_sizes=[64, 64, 32, 16]),
@@ -179,7 +180,8 @@ MODEL_CONFIGURATIONS = {
                                       combined_object_layer_sizes=[64, 32]),
         models.TransformerModel: dict(embedding_size=16, num_transformer_layers=3, num_heads=2,
                                       transformer_mlp_sizes=[16, 16], mlp_sizes=[64, 32]),
-        models.CNNModel: dict(conv_sizes=[8, 16, 32], mlp_sizes=[32, 32],),
+        # models.CNNModel: dict(conv_sizes=[8, 16, 32], mlp_sizes=[32, 32],),
+        models.SimplifiedCNNModel: dict(conv_sizes=[8, 16, 32], mlp_sizes=[32, 32],),
     }
 }
 parser.add_argument('--model-configuration', type=str, action='append', choices=list(MODEL_CONFIGURATIONS.keys()),
