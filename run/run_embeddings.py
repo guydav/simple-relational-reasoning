443bec0e65b7c36590e0c40e26f20dc61a9b0594
Guy Davidson <guydav@gmail.com>
2022-03-16 10:58:02 -0400

    Cleaned up embedding experiment logic

--- run/run_embeddings.py
+++ run/run_embeddings.py
@@ -4,6 +4,7 @@ import copy
 import itertools
 import os
 import sys
+from tqdm import tqdm
 
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
@@ -29,21 +30,28 @@ parser.add_argument('--replications', type=int, default=1, help='Number of repli
 DEFAULT_N = 1024
 parser.add_argument('-n', '--n-examples', type=int, default=DEFAULT_N, help='Number of examples to generate')
 
-parser.add_argument('-s', '--stimulus-generator', type=str, 
-    choices=list(STIMULUS_GENERATORS.keys()), help='Stimulu generator to run with')
+parser.add_argument('-s', '--stimulus-generator', action='append', 
+    choices=list(STIMULUS_GENERATORS.keys()), help='Stimulus generator to run with')
 
 parser.add_argument('--stimulus-generator-kwargs', action='append', 
     Help='Specify key=value pairs to pass to the stimulus generator.')
 
-DEFAULT_DISTANCE_ENDPOINTS = (30, 70)
+DEFAULT_DISTANCE_ENDPOINTS = (-1, -1)
+DISTANCE_ENDPOINTS_DICT = {  # two_reference_objects, adjacent_reference_objects
+    (False, False): (30, 80),
+    (True, False): (50, 80),
+    (True, True): (40, 80),  
+}
 parser.add_argument('--target-distance-endpoints', type=int, nargs=2, default=DEFAULT_DISTANCE_ENDPOINTS,)
-
 
 parser.add_argument('-r', '--relation', type=str, action='append', choices=RELATIONS,
                     help='Which relation(s) to run (default: all)')
 
 DEFAULT_TWO_REFERENCE_OBJECTS = None
 parser.add_argument('--two-reference-objects', type=int, default=DEFAULT_TWO_REFERENCE_OBJECTS)
+
+DEFAULT_ADJACENT_REFERENCE_OBJECTS = None
+parser.add_argument('--adjacent-reference-objects', type=int, default=DEFAULT_ADJACENT_REFERENCE_OBJECTS)
 
 DEFAULT_TRANSPOSE = None
 parser.add_argument('--transpose-stimuli', type=int, default=DEFAULT_TRANSPOSE)
@@ -73,56 +81,44 @@ parser.add_argument('-o', '--output-file', type=str, help='Output file to write 
 
 parser.add_argument('--rotate-angle', type=int, default=None, help='Angle to rotate the stimuli by')
 
+parser.add_argument('--tqdm', action='store_true', help='Use tqdm progress bar')
 
 MULTIPLE_OPTION_FIELD_DEFAULTS = {
     'relation': RELATIONS,
     'two_reference_objects': [0, 1],
-    'transpose_stimuli': [0, 1],
+    'adjacent_reference_objects': [0, 1],
     'n_target_types': VALID_N_TARGET_TYPES,
+    'transpose_stimuli': [0, 1],
     'n_habituation_stimuli': [1, 4,],
     'rotate_angle': [-45, -30, 0, 30, 45],
 }
 MULTIPLE_OPTION_REWRITE_FIELDS = list(MULTIPLE_OPTION_FIELD_DEFAULTS.keys())
 
-def default_name_func(generator_kwargs, base_name=''):
-    two_reference_objects = generator_kwargs['two_reference_objects']
-    transpose = generator_kwargs['transpose']
-    
-    if two_reference_objects:
-        if transpose:
-            rel_name = 'VerticalBetween'
-        else:
-            rel_name = 'Between'
-    else:
-        if transpose:
-            rel_name = 'Left/Right'
-        else:
-            rel_name = 'Above/Below'
-    
-    name = f'{base_name}{base_name and "-" or ""}{rel_name}'
-            
-    n_target_types = None
-    if 'n_target_types' in generator_kwargs:
-        n_target_types = generator_kwargs['n_target_types']
-        return name + f'-{n_target_types}-types'
-    
-    return name
+SINGLE_OPTION_FIELDS_TO_DF = ['seed', 'n_examples', ]
 
 
-def create_generators_and_names(triplet_generator_class, stimulus_generator, kwarg_value_sets, 
-                                name_func=default_name_func, name_func_kwargs=None):
+def create_triplet_generators(args, name_func_kwargs=None):
     if name_func_kwargs is None:
         name_func_kwargs = {}
     
-    names = []
+    triplet_generator_class = TRIPLET_GENERATORS[args.triplet_generator]
+
     triplet_generators = []
 
-    for value_set in itertools.product(*kwarg_value_sets.values()):
-        kwargs = {k: v for (k, v) in zip(kwarg_value_sets.keys(), value_set)}
-        names.append(name_func(kwargs, **name_func_kwargs))
-        triplet_generators.append(triplet_generator_class(stimulus_generator, **kwargs))
+    for stimulus_generator_name in args.stimulus_generator:
+        stimulus_generator_builder = STIMULUS_GENERATORS[stimulus_generator_name]        
+        stimulus_generator = stimulus_generator_builder(**args.stimulus_generator_kwargs)
 
-    return names, triplet_generators
+        triplet_generator = triplet_generator_class(stimulus_generator,args.distance_endpoints,
+            relation=args.relation, two_reference_objects=args.two_reference_objects, 
+            adjacent_reference_objects=args.adjacent_reference_objects,
+            n_target_types=args.n_target_types, transpose=args.transpose_stimuli,
+            n_habituation_stimuli=args.n_habituation_stimuli, 
+            multiple_habituation_radius=args.multiple_habituation_radius)
+        
+        triplet_generators.append(triplet_generator)
+
+    return triplet_generators
     
 
 def handle_multiple_option_defaults(args):
@@ -138,6 +134,8 @@ def handle_multiple_option_defaults(args):
 
 
 def handle_single_args_setting(args):
+    var_args = vars(args)
+    
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
         device = torch.device('cuda:0')
@@ -158,21 +156,18 @@ def handle_single_args_setting(args):
     model_names = [ f'{d["name"]}-{"saycam({s})".format(s=d["syacam"]) if "saycam" in d else (d["pretrained"] and "imagenet" or "random")}'
                for d in model_kwarg_dicts]
 
-    stimulus_generator_builder = STIMULUS_GENERATORS[args.stimulus_generator]
-    stimulus_generator_kwargs = {s.split('=')[0]: s.split('=')[1] for s in args.stimulus_generator_kwargs}
+    var_args['stimulus_generator_kwargs'] = {s.split('=')[0]: s.split('=')[1] for s in args.stimulus_generator_kwargs}
 
     all_model_results = []
-    for r in range(args.n_replications):
-        stimulus_generator_kwargs['rng'] = np.random.default_rng(args.seed + r)
-        stimulus_generator_kwargs['rotate_angle'] = args.rotate_angle
-        stimulus_generator = stimulus_generator_builder(**stimulus_generator_kwargs) 
+    for r in range(args.replications):
+        var_args['seed'] = args.seed + 1
+        var_args['stimulus_generator_kwargs']['rng'] = np.random.default_rng(args.seed)
+        var_args['stimulus_generator_kwargs']['rotate_angle'] = args.rotate_angle
 
-        value_sets = {key: getattr(args, key) if getattr(args, key) is not None else default_value
-            for key, default_value in MULTIPLE_OPTION_FIELD_DEFAULTS.items()}
+        if args.distance_endpoints == DEFAULT_DISTANCE_ENDPOINTS:
+            var_args['distance_endpoints'] = DISTANCE_ENDPOINTS_DICT[bool(args.two_reference_objects), bool(args.adjacent_reference_objects)]
 
-        triplet_names, triplet_generators = create_generators_and_names(
-            TRIPLET_GENERATORS[args.triplet_generator], stimulus_generator, 
-            value_sets, name_func_kwargs=dict(base_name=args.base_model_name))
+        triplet_names, triplet_generators = create_triplet_generators(args)
 
         all_model_results.append(run_multiple_models_multiple_generators(
             model_names, model_kwarg_dicts, triplet_names, triplet_generators, args.n_examples))
@@ -183,15 +178,17 @@ def handle_single_args_setting(args):
     result_df = table_per_relation_multiple_results(all_model_results, N=args.n_examples,
         display_tables=False)
 
-    for multiple_option_key in MULTIPLE_OPTION_REWRITE_FIELDS:
-        result_df[multiple_option_key] = args.__dict__[multiple_option_key]
+    for key in MULTIPLE_OPTION_REWRITE_FIELDS + SINGLE_OPTION_FIELDS_TO_DF:
+        result_df[key] = var_args[key]
 
-    result_df.to_csv(args.output_file)
+    return result_df
 
 
 if __name__ == '__main__':
     main_args = parser.parse_args()
+    main_args = handle_multiple_option_defaults(main_args)
     main_var_args = vars(main_args)
+
     print(' ' * 26 + 'Global Options')
     for k, v in main_var_args.items():
         print(' ' * 26 + k + ': ' + str(v))
@@ -200,13 +197,22 @@ if __name__ == '__main__':
 
     dataframes = []
 
-    for value_combination in itertools.product(*multiple_option_field_values):
+    value_iter = itertools.product(*multiple_option_field_values)
+    if main_args.tqdm:
+        total = np.prod([len(v) for v in multiple_option_field_values])
+        value_iter = tqdm(value_iter, desc='Setting', total=total)
+
+    for value_combination in value_iter:
         args_copy = copy.deepcopy(main_args)
         var_args_copy = vars(args_copy)
         var_args_copy.update({key: value for key, value in zip(MULTIPLE_OPTION_REWRITE_FIELDS,
                                                                value_combination)})
-    
+
         # TODO: any checks for arg combinations we shouldn't run?
+        if not args_copy.two_reference_objects and args_copy.adjacent_reference_objects:
+            print(f'Skipping because adjacent_reference_object={args_copy.adjacent_reference_objects} and two_reference_objects={args_copy.two_reference_objects}')
+            continue
+
         dataframes.append(handle_single_args_setting(args_copy))
 
     out_df = pd.concat(dataframes)
