66c99cc7c052236006df18d9f3f8009a862017ce
guydav <gd1279@nyu.edu>
2022-03-24 09:31:54 -0400

    Moved old data to buggy folder to run new data

--- run/run_embeddings.sh
+++ run/run_embeddings.sh
@@ -1,9 +1,11 @@
 python3.7 run/run_embeddings.py \
     --triplet-generator "quinn" \
+    --stimulus-generators "different_shapes" \
     --stimulus-generators "split_text" \
-    --model "resnext" \
+    --stimulus-generators "random_color" \
+    --model "mobilenet" \
     --saycam "S"  \
     --batch-size 32 \
-    --output-file "embedding_outputs/saycam_split_text_original_test.csv" \
+    --output-file "embedding_outputs/saycam_nan_test.csv" \
     --tqdm \
     --print-setting-options
