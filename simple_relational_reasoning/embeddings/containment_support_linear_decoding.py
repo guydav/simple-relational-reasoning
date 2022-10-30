ab5a2150212b6f24872db0683b515d9d805b8f8f
Guy Davidson <guydav@gmail.com>
2022-10-30 18:15:34 -0400

    Added a patience feature

--- simple_relational_reasoning/embeddings/containment_support_linear_decoding.py
+++ simple_relational_reasoning/embeddings/containment_support_linear_decoding.py
@@ -17,6 +17,7 @@ import os
 from .task import METRICS, Metric, TaskResults
 
 BATCH_SIZE = 32
+DEFAULT_PATIENCE_EPOCHS = 5
 
 
 class LinearClassifier(nn.Module):
@@ -46,6 +47,7 @@ def _weigted_random_sampler(labels: torch.Tensor) -> WeightedRandomSampler:
 def containment_support_linear_decoding_single_model_single_feature(
     model: nn.Module, datasets: DecodingDatasets, 
     n_epochs: int, lr: float,
+    patience_epochs: int = DEFAULT_PATIENCE_EPOCHS,
     batch_size: int = BATCH_SIZE, 
     device: typing.Optional[torch.device] = None, 
     ):
@@ -74,6 +76,7 @@ def containment_support_linear_decoding_single_model_single_feature(
 
     best_decoder = None
     min_val_loss = np.inf
+    min_val_epoch = np.inf
 
     for epoch in range(n_epochs):
         epoch_train_losses = []
@@ -124,12 +127,17 @@ def containment_support_linear_decoding_single_model_single_feature(
         val_losses.append(epoch_val_loss)
         val_accuracies.append(epoch_val_acc)
 
-        print(f'After epoch {epoch}: train loss {epoch_train_loss:.3f}, train acc {epoch_train_acc:.3f}, val loss {epoch_val_loss:.3f}, val acc {epoch_val_acc:.3f}')
+        print(f'After epoch {epoch}: train loss {epoch_train_loss:.4f}, train acc {epoch_train_acc:.4f}, val loss {epoch_val_loss:.4f}, val acc {epoch_val_acc:.4f}')
 
         if epoch_val_loss < min_val_loss:
-            print(f'New best val loss {epoch_val_loss:.3f} < {min_val_loss:.3f}, copying decoder')
+            print(f'New best val loss {epoch_val_loss:.4f} < {min_val_loss:.4f}, copying decoder')
             min_val_loss = epoch_val_loss
+            min_val_epoch = epoch
             best_decoder = copy.deepcopy(decoder).cpu()
+
+        elif epoch - min_val_epoch > patience_epochs:
+            print(f'No improvement in val loss for {patience_epochs} epochs, stopping')
+            break
 
     best_decoder = typing.cast(nn.Module, best_decoder).to(device).eval()
 
@@ -144,11 +152,11 @@ def containment_support_linear_decoding_single_model_single_feature(
             logits = best_decoder(embeddings)
             loss = criterion(logits, y)
             test_losses.append(loss.item())
-            test_accs.append((logits.argmax(dim=1) == y).float().mean())
+            test_accs.append((logits.argmax(dim=1) == y).float().mean().item())
 
     test_loss = np.mean(test_losses)
     test_acc = np.mean(test_accs)
-    print(f'With the best model, test loss {test_loss:.3f}, test acc {test_acc:.3f}')
+    print(f'With the best model, test loss {test_loss:.4f}, test acc {test_acc:.4f}')
 
     del train_dataloader
     del val_dataloader
@@ -167,7 +175,7 @@ def run_containment_support_linear_decoding_single_model_multiple_features(
     model: nn.Module, dataset: ContainmentSupportDataset, 
     n_epochs: int, lr: float, by_target_object: bool, by_reference_object: bool,
     batch_size: int = BATCH_SIZE, validation_proportion: float = DEFAULT_VALIDATION_PROPORTION,
-    random_seed: int = DEFAULT_RANDOM_SEED, ):
+    patience_epochs: int = DEFAULT_PATIENCE_EPOCHS, random_seed: int = DEFAULT_RANDOM_SEED, ):
 
     if by_target_object == by_reference_object:
         raise ValueError('Exactly one of by_target_object and by_reference_object must be True')
@@ -177,7 +185,7 @@ def run_containment_support_linear_decoding_single_model_multiple_features(
     if by_target_object:
         for target_object in dataset.target_objects:
             decoding_datasets = dataset.generate_decoding_datasets(test_target_object=target_object, validation_proportion=validation_proportion, random_seed=random_seed)
-            feature_results = containment_support_linear_decoding_single_model_single_feature(model, decoding_datasets, n_epochs, lr, batch_size)
+            feature_results = containment_support_linear_decoding_single_model_single_feature(model, decoding_datasets, n_epochs, lr, patience_epochs, batch_size)
             feature_results['test_target_object'] = target_object
             feature_results['test_type'] = 'target_object'
             model_results.append(feature_results)
@@ -185,7 +193,7 @@ def run_containment_support_linear_decoding_single_model_multiple_features(
     else:
         for reference_object in dataset.reference_objects:
             decoding_datasets = dataset.generate_decoding_datasets(test_reference_object=reference_object, validation_proportion=validation_proportion, random_seed=random_seed)
-            feature_results = containment_support_linear_decoding_single_model_single_feature(model, decoding_datasets, n_epochs, lr, batch_size)
+            feature_results = containment_support_linear_decoding_single_model_single_feature(model, decoding_datasets, n_epochs, lr, patience_epochs, batch_size)
             feature_results['test_reference_object'] = reference_object
             feature_results['test_type'] = 'reference_object'
             model_results.append(feature_results)
@@ -199,16 +207,19 @@ def run_containment_support_linear_decoding_multiple_models(
     dataset: ContainmentSupportDataset, 
     n_epochs: int, lr: float, by_target_object: bool, by_reference_object: bool,
     batch_size: int = BATCH_SIZE, validation_proportion: float = DEFAULT_VALIDATION_PROPORTION,
-    random_seed: int = DEFAULT_RANDOM_SEED, ):
+    patience_epochs: int = DEFAULT_PATIENCE_EPOCHS, random_seed: int = DEFAULT_RANDOM_SEED, ):
 
     all_model_results = []
+
+    if by_reference_object == by_target_object:
+        raise ValueError('Must set exactly one of by_reference_object and by_target_object to True')
     
     for name, model_kwargs in zip (model_names, model_kwarg_dicts):
         print(f'Starting model {name}')
         model = build_model(**model_kwargs)
 
         model_results = run_containment_support_linear_decoding_single_model_multiple_features(
-            model, dataset, n_epochs, lr, by_target_object, by_reference_object, batch_size, validation_proportion, random_seed)
+            model, dataset, n_epochs, lr, by_target_object, by_reference_object, batch_size, validation_proportion, patience_epochs, random_seed)
 
         for feature_result in model_results:
             feature_result['model'] = name
@@ -220,11 +231,3 @@ def run_containment_support_linear_decoding_multiple_models(
         torch.cuda.empty_cache()
         
     return all_model_results
-
-
-
-
-
-
-    if by_reference_object == by_target_object:
-        raise ValueError('Must set exactly one of by_reference_object and by_target_object to True')
