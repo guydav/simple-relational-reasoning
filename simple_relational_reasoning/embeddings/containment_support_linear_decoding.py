39cd5fc0e123d796327a3788475d3b9719c93239
Guy Davidson <guydav@gmail.com>
2022-11-01 12:33:53 -0400

    Fixed sanity check

--- simple_relational_reasoning/embeddings/containment_support_linear_decoding.py
+++ simple_relational_reasoning/embeddings/containment_support_linear_decoding.py
@@ -188,7 +188,7 @@ def run_containment_support_linear_decoding_single_model_multiple_features(
     if by_target_object is None and by_reference_object is None and test_proportion is None:
             raise ValueError('test_reference_object, test_target_object, and test_proportion cannot all be None')
 
-    if int(by_target_object is not None) + int(by_reference_object is not None) + int(test_proportion is not None) > 1:
+    if int(by_target_object) + int(by_reference_object) + int(test_proportion is not None) > 1:
         raise ValueError('Only one of test_reference_object, test_target_object, and test_proportion can be specified')
 
     model_results = []
@@ -230,7 +230,7 @@ def run_containment_support_linear_decoding_multiple_models(
     if by_target_object is None and by_reference_object is None and test_proportion is None:
             raise ValueError('test_reference_object, test_target_object, and test_proportion cannot all be None')
 
-    if int(by_target_object is not None) + int(by_reference_object is not None) + int(test_proportion is not None) > 1:
+    if int(by_target_object) + int(by_reference_object) + int(test_proportion is not None) != 1:
         raise ValueError('Only one of test_reference_object, test_target_object, and test_proportion can be specified')
 
     
