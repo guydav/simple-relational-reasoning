d6624f1abf8ab9e0de61c2ae5ea12e73b24ab775
Guy Davidson <guydav@gmail.com>
2022-11-04 16:58:29 -0400

    Implemented tsne mode for thse stimuli

--- simple_relational_reasoning/embeddings/containment_support_task.py
+++ simple_relational_reasoning/embeddings/containment_support_task.py
@@ -28,9 +28,6 @@ def containment_support_task_single_model(
     if device is None:
         device = next(model.parameters()).device
 
-    if tsne_mode:
-        raise NotImplementedError('TSNE mode not implemented yet for containment support task')
-
     B = batch_size
     dataloader = DataLoader(TensorDataset(dataset.dataset), batch_size=batch_size, shuffle=False)
     
@@ -46,34 +43,32 @@ def containment_support_task_single_model(
         data_iter = tqdm(dataloader, desc='Batches')
 
     all_results = []
-        
+    output_embeddings = []
+
     for b in data_iter:
-        # if tsne_mode:
-        #     x = b[0]  # shape (B, 3, 224, 224) where H is the number of habituation stimuli
-        #     x = x.to(device)
-        #     e = model(x).detach().cpu().numpy()
-        #     output_embeddings.append(e)
+        if tsne_mode:
+            x = b[0]  # shape (B, K, 3, 224, 224) 
+            B, K = x.shape[:2]
+            x = x.view(-1, *x.shape[2:])
+            x = x.to(device)
+            e = model(x).detach().view(B, K, x.shape[1:]).cpu().numpy()
+            output_embeddings.append(e)
 
-        # else:
-        x = b[0]  # shape (B, 4, 3, 224, 224) where H is the number of habituation stimuli
-        x = x.view(-1, *x.shape[2:])
-        x = x.to(device)
-        e = model(x).detach()
-        e = e.view(B, n_stimuli_per_batch, -1)  # shape (B, H + 2, Z)
-        
-        # if H > 1:  # if we have multiple habituation stimuli, average them
-        #     average_habituation_embedding = e[:, :-2, :].mean(dim=1, keepdim=True)
-        #     test_embeddings = e[:, -2:]
-        #     e = torch.cat((average_habituation_embedding, test_embeddings), dim=1)
-
-        embedding_pairwise_cosine = cos(e[:, :, None, :], e[:, None, :, :])  # shape (B, K , K) where K = 3 for 3 stimuli and K = 6 for 4 stimuli
-        triplet_cosines = embedding_pairwise_cosine[:, triangle_indices[0], triangle_indices[1]] # shape (B, K)
-
-        if aggregate_results:
-            for metric in metrics:
-                model_results[metric.name].append(metric(triplet_cosines).cpu().numpy())
         else:
-            all_results.append(triplet_cosines.cpu())
+            x = b[0]  # shape (B, 4, 3, 224, 224) where H is the number of habituation stimuli
+            x = x.view(-1, *x.shape[2:])
+            x = x.to(device)
+            e = model(x).detach()
+            e = e.view(B, n_stimuli_per_batch, -1)  # shape (B, H + 2, Z)
+
+            embedding_pairwise_cosine = cos(e[:, :, None, :], e[:, None, :, :])  # shape (B, K , K) where K = 3 for 3 stimuli and K = 6 for 4 stimuli
+            triplet_cosines = embedding_pairwise_cosine[:, triangle_indices[0], triangle_indices[1]] # shape (B, K)
+
+            if aggregate_results:
+                for metric in metrics:
+                    model_results[metric.name].append(metric(triplet_cosines).cpu().numpy())
+            else:
+                all_results.append(triplet_cosines.cpu())
 
     if aggregate_results and not tsne_mode:
         for metric in metrics:
@@ -85,6 +80,9 @@ def containment_support_task_single_model(
     #     return data.cpu().numpy(), np.concatenate(output_embeddings, axis=0)
     # del data
     
+    if tsne_mode:
+        return np.concatenate(output_embeddings, axis=0)
+
     if not aggregate_results:
         return np.concatenate(all_results, axis=0)
 
