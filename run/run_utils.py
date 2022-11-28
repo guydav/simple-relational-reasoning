a0323338ebf775376628c3075c922867457dd696
Guy Davidson <guydav@gmail.com>
2022-11-28 11:50:57 +0200

    Forgot I updated a parameter name

--- run/run_utils.py
+++ run/run_utils.py
@@ -15,27 +15,27 @@ def args_to_model_configurations(args: argparse.Namespace) -> typing.Tuple[typin
 
     for model_name in args.model:
         if model_name in BASELINE_MODELS and args.saycam:
-            model_kwarg_dicts.append(dict(name=model_name, device=args.device, pretrained=False, saycam=args.saycam, unpooled_output=args.unpooled_output))
+            model_kwarg_dicts.append(dict(model_name=model_name, device=args.device, pretrained=False, saycam=args.saycam, unpooled_output=args.unpooled_output))
             model_names.append(f'{model_name}-saycam({args.saycam})')
         
         if model_name in BASELINE_MODELS and args.imagenet:
-            model_kwarg_dicts.append(dict(name=model_name, device=args.device, pretrained=True, unpooled_output=args.unpooled_output))
+            model_kwarg_dicts.append(dict(model_name=model_name, device=args.device, pretrained=True, unpooled_output=args.unpooled_output))
             model_names.append(f'{model_name}-imagenet')
 
         if model_name in BASELINE_MODELS and args.untrained:
-            model_kwarg_dicts.append(dict(name=model_name, device=args.device, pretrained=False, unpooled_output=args.unpooled_output))
+            model_kwarg_dicts.append(dict(model_name=model_name, device=args.device, pretrained=False, unpooled_output=args.unpooled_output))
             model_names.append(f'{model_name}-random')
 
         if model_name in FLIPPING_MODELS and args.flipping and len(args.flipping) > 0:
             for flip_type in args.flipping:
-                model_kwarg_dicts.append(dict(name=model_name, device=args.device, 
+                model_kwarg_dicts.append(dict(model_name=model_name, device=args.device, 
                     pretrained=False, flip=flip_type, unpooled_output=args.unpooled_output))
 
                 model_names.append(f'{model_name}-saycam(S)-{flip_type}')
 
         if model_name in DINO_MODELS and args.dino and len(args.dino) > 0:
             for dino in args.dino:
-                model_kwarg_dicts.append(dict(name=model_name, device=args.device, 
+                model_kwarg_dicts.append(dict(model_name=model_name, device=args.device, 
                     pretrained=False, dino=dino, unpooled_output=args.unpooled_output))
 
                 model_names.append(f'{model_name}-DINO-{dino}')
